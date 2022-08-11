0121cc7d7054b7395d653c309ef9545a747a6979
Ruslan <lkuraer@gmail.com>
2022-08-11 14:28:59 +0500

    Initial Commit

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Ruslan Sabirov on 11.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
