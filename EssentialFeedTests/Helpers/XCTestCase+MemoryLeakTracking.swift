5a87cd433763b08595ff83550c8079ab0d671216
lkuraer <lkuraer@gmail.com>
2022-09-07 13:52:23 +0500

    minor refactor of Date extension

--- EssentialFeedTests/Helpers/XCTestCase+MemoryLeakTracking.swift
+++ EssentialFeedTests/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -17,13 +17,19 @@ extension XCTestCase {
  
 extension Date {
     func minusMaxAge() -> Date {
-        return adding(days: -7)
+        return adding(days: -maxAge)
     }
     
-    func adding(days: Int) -> Date {
+    private var maxAge: Int {
+        return 7
+    }
+    
+    private func adding(days: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
-    
+}
+
+extension Date {
     func adding(seconds: TimeInterval) -> Date {
         self + seconds
     }
